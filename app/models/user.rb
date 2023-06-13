cf35bf74d0702e8de2194e8414b87e2c1908777a
misha <mishabansal7@gmail.com>
2023-06-13 15:02:15 -0700

    more backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  display_name    :string
+#  full_name       :string           not null
+#  title           :string
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
 
-  validates :username, :session_token, presence: true, uniqueness: true
-  validates :password_digest, presence: true
-  validates :password, length: {minimum: 6}, allow_nil: true
+  before_validation :ensure_session_token
+
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :email, 
+    length: { in: 3..30 },
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password_digest, :full_name, presence: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
 
   has_secure_password
 
-  def self.find_by_credentials(username, password)
-    user = User.find_by(username: username)
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
 
     # if user&.authenticate(password) # equivalent
     if user && user.authenticate(password)
