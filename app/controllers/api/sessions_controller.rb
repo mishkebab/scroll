4508025f3d92098f71faf069a5a0db1757b17251
misha <mishabansal7@gmail.com>
2023-06-13 21:51:45 -0700

    adding user controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -12,7 +12,7 @@ class Api::SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by_credentials(params[:username], params[:password])
+        @user = User.find_by_credentials(params[:email], params[:password])
         if @user
             login!(@user)
             render `api/users/show`
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,21 +1,21 @@
 class Api::UsersController < ApplicationController
-    # wrap_parameters include: User.attribute_names + ['password']
+    wrap_parameters include: User.attribute_names + ['password']
     
-    # before_action :require_logged_out
+    before_action :require_logged_out
 
-    # def create
-    #     @user = User.new(user_params)
+    def create
+        @user = User.new(user_params)
 
-    #     if @user.save
-    #         login!(@user)
-    #         render: show
-    #     else
-    #         render json: @user.errors.full_messages, status: 422
-    #     end
-    # end 
+        if @user.save
+            login!(@user)
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end 
 
-    # private
-    # def user_params
-    #     params.require(:user).permit(:email, :password)
-    # end 
+    private
+    def user_params
+        params.require(:user).permit(:email, :full_name, :title, :display_name, :password)
+    end 
 end
